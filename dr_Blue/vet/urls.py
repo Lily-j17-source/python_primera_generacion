9770f7420ba68540efefb9d8b9bd119002e9b910
Lily-j17-source <liliana.1793@hotmail.com>
2021-03-17 23:46:55 -0600

    modelos basados en vista, se agrego lista de owners, agrego template de owners

--- dr_Blue/vet/urls.py
+++ dr_Blue/vet/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
-from .views import list_pet_owners
+from .views import Owners, Test, OwnersList, OwnersDetail
 
-urlpatterns = [path("owners/", list_pet_owners)]
+urlpatterns = [
+    path("owners/", OwnersList.as_view()),
+    path("owners/<int:pk>/", OwnersDetail.as_view()),
+    path("test/", Test.as_view()),
+]
